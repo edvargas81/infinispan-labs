d4496840695bbfeb5f2e4e3a5a90562d1bee17e8
Galder Zamarreño <galder@zamarreno.com>
2012-01-20 14:26:17 +0000

    Update to use new configuration API

--- lab1/src/main/java/org/infinispan/labs/lab1/service/InfinispanTicketService.java
+++ lab1/src/main/java/org/infinispan/labs/lab1/service/InfinispanTicketService.java
@@ -34,7 +34,7 @@ import javax.inject.Inject;
 import javax.inject.Named;
 
 import org.infinispan.Cache;
-import org.infinispan.config.Configuration.CacheMode;
+import org.infinispan.configuration.cache.CacheMode;
 import org.infinispan.labs.lab1.TicketPopulator;
 import org.infinispan.labs.lab1.model.TicketAllocation;
 
@@ -85,14 +85,14 @@ public class InfinispanTicketService implements TicketService {
    }
 
    public String getNodeId() {
-      if (tickets.getConfiguration().getCacheMode() != CacheMode.LOCAL)
+      if (isCacheClustered())
          return tickets.getAdvancedCache().getCacheManager().getAddress().toString();
       else
          return "local cache";
    }
 
    public String getOwners(String key) {
-      if (tickets.getConfiguration().getCacheMode() != CacheMode.LOCAL) {
+      if (isCacheClustered()) {
          return asCommaSeparatedList(tickets.getAdvancedCache().getDistributionManager().locate(key));
       } else {
          return asCommaSeparatedList(Collections.singletonList("local"));
@@ -101,6 +101,10 @@ public class InfinispanTicketService implements TicketService {
 
    public TicketAllocation getTicketAllocation(String id) {
       return tickets.get(id);
+   }
+
+   private boolean isCacheClustered() {
+      return tickets.getCacheConfiguration().clustering().cacheMode() != CacheMode.LOCAL;
    }
 
    private static String asCommaSeparatedList(List<?> objects) {
