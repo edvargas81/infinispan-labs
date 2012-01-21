66cdf252b43757c3ff9c4bbaad5742b86588a367
Galder Zamarreño <galder@zamarreno.com>
2012-01-21 16:29:31 +0000

    Use TCP based jgroups stack and make test-network script point to it

--- nic-test/test-network.sh
+++ nic-test/test-network.sh
@@ -2,6 +2,6 @@
 
 DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-java -cp $DIR/jgroups.jar -Djgroups.bind_addr=127.0.0.1 -Djava.net.preferIPv4Stack=true org.jgroups.demos.Draw &
-java -cp $DIR/jgroups.jar -Djgroups.bind_addr=127.0.0.1 -Djava.net.preferIPv4Stack=true org.jgroups.demos.Draw &
+java -cp $DIR/jgroups.jar -Djgroups.bind_addr=127.0.0.1 -Djava.net.preferIPv4Stack=true org.jgroups.demos.Draw -props $DIR/../lab1/src/sample/jgroups.xml &
+java -cp $DIR/jgroups.jar -Djgroups.bind_addr=127.0.0.1 -Djava.net.preferIPv4Stack=true org.jgroups.demos.Draw -props $DIR/../lab1/src/sample/jgroups.xml &
 
